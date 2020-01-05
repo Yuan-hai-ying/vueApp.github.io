b2c87fb0a64349188f3808d607240da489365f96
18627805032@163.com <18627805032@163.com>
2020-01-05 18:45:52 +0800

    首页制作

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,79 @@
 <template>
-  <div>
-    <h1>这是 App 组不不不件去去去</h1>
+  <div class="app-container">
+
+    <!-- 顶部 Header 区域 -->
+    <mt-header fixed title="嘉时代"></mt-header>
+
+
+    <!-- 中间的 路由 router-view 区域 -->
+		<transition>
+			<router-view></router-view>
+		</transition>
+
+
+    <!-- 底部 Tabbar 区域 -->
+    <nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item" to="/home">
+				<span class="mui-icon mui-icon-home" ></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/shopcar">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+					<span class="mui-badge">0</span>
+				</span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-search"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
+
   </div>
 </template>
 
 <script>
+      export default {
+		  data(){
+             return{
+
+			 }
+		  },
+		  created(){
+			  localStorage.setItem('interface', 'http://vue.studyit.io')
+		  }
+	  }
 </script>
 
 
 <style lang="scss" scoped>
-
+.mint-header{
+  z-index: 99;
+  background-color:red;
+  
+}
+.app-container{
+	padding:40px 0 ;
+	overflow:hidden;
+}
+.v-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.v-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+  position: absolute;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease;
+}
+.mui-bar-tab .mui-tab-item.mui-active{
+	color:red;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,2 +1,51 @@
 // 入口文件
-console.log('OK')
+import Vue from 'vue'
+
+// 1.1 导入路由的包
+import VueRouter from 'vue-router'
+// 1.2 安装路由
+Vue.use(VueRouter)
+
+// 导入vue axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+axios.defaults.baseURL = 'http://www.liulongbin.top:3005/'
+// import VueAxios from 'vue-axios'
+// 设置请求的根路径
+// axios.defaults.baseURL= 'http://vue.studyit.io'
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+// Vue.use(axios);
+Vue.prototype.http = axios;　
+// import VueResource from 'vue-resource'
+// 2.2 安装 vue-resource
+// Vue.use(VueResource)
+// 设置请求的根路径
+// Vue.http.options.root = 'http://vue.studyit.io';
+// 全局设置 post 时候表单数据格式组织形式   application/x-www-form-urlencoded
+// Vue.http.options.emulateJSON = true;
+// 导入mui
+import './lib/mui/css/mui.min.css'
+// 导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
+
+// 按需导入mintUI
+import {Header,Swipe, SwipeItem} from 'mint-ui'
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Header.name,Header)
+// import 'mint-ui/lib/style.css'
+
+// 1.3 导入自己的 router.js 路由模块
+import router from './router.js'
+
+// 导入跟组件
+import app from './App.vue'
+
+var vm=new Vue({
+    el:'#app',
+    render:c=>c(app),
+    router
+
+})
